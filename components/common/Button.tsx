eb876cdd79fa6008464662f272ea541b378ea280
kwansikdev <kwansk0424@gmail.com>
2021-07-16 14:25:51 +0900

    feat: Add redux setting & register's style

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -2,67 +2,92 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import palette from '../../styles/palette';
 
-const normalButtonStyle = css`
-  background-color: ${palette.bittersweet};
+// 버튼 색상 구하기
+const getButtonColor = (color: string, colorReverse: boolean) => {
+  if (colorReverse) {
+    switch (color) {
+      case 'dark_cyan':
+        return css`
+          background-color: #fff;
+          border: 2px solid ${palette.dark_cyan};
+          color: ${palette.dark_cyan};
+        `;
+      default:
+        return css`
+          background-color: #fff;
+          border: 2px solid ${palette.black};
+          color: ${palette.black};
+        `;
+    }
+  }
+
+  switch (color) {
+    case 'dark_cyan':
+      return css`
+        background-color: ${palette.dark_cyan};
+        color: #fff;
+      `;
+    case 'bittersweet':
+      return css`
+        background-color: ${palette.bittersweet};
+        color: #fff;
+      `;
+    default:
+      return css`
+        background-color: #fff;
+        border: 1px solid ${palette.gray_c4};
+        color: ${palette.black};
+      `;
+  }
+};
+
+interface StyledButtonProps {
+  width: string | undefined;
+  colorReverse: boolean;
+}
+
+const Container = styled.button<StyledButtonProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 48px;
   padding: 0 15px;
   border: 0;
   border-radius: 4px;
-  outline: none;
-  cursor: pointer;
-  color: ${palette.gray_48};
   font-size: 18px;
   font-weight: 700;
-`;
-
-const RegisterButtonStyle = css`
-  background-color: #fff;
-  width: 161px;
-  height: 45px;
-  border: 1px solid ${palette.gray_c4};
-  border-radius: 4px;
   outline: none;
   cursor: pointer;
-  color: ${palette.gray_48};
-  font-size: 18px;
-  font-weight: 700;
-`;
 
-const Container = styled.button<{ styleType: 'normal' | 'register' }>`
-  ${({ styleType }) => (styleType === 'normal' ? normalButtonStyle : RegisterButtonStyle)}
-  ${(props) => getButtonColor(props.color || '')};
-`;
+  width: ${(props) => props.width};
 
-// 버튼 색상 구하기
-const getButtonColor = (color: string) => {
-  switch (color) {
-    case 'dark_cyan':
-      return css`
-        /* border: 2px solid ${palette.dark_cyan}; */
-        color: #fff;
-        background-color: ${palette.dark_cyan};
-      `;
-    case 'white':
-      return css`
-        background-color: #fff;
-      `;
-    default:
-      return css`
-        background-color: ${palette.bittersweet};
-      `;
+  ${(props) => getButtonColor(props.color || '', props.colorReverse)};
+
+  svg {
+    margin-right: 12px;
   }
-};
+`;
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  color?: 'dark_cyan' | 'white';
-  styleType?: 'normal' | 'register';
+  color?: 'dark_cyan' | 'white' | 'bittersweet';
+  width?: string;
+  colorReverse?: boolean;
+  icon?: JSX.Element;
 }
 
-const Button: React.FC<Props> = ({ children, color, styleType = 'normal', ...props }) => {
+const Button: React.FC<Props> = ({
+  children,
+  color,
+  width,
+  colorReverse = false,
+  icon,
+  ...props
+}) => {
   return (
-    <Container {...props} color={color} styleType={styleType}>
+    <Container {...props} color={color} width={width} colorReverse={colorReverse}>
+      {icon}
       {children}
     </Container>
   );
