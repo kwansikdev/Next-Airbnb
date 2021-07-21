4e65d1850019a6931fee7c7e041d883938f81fb1
kwansikdev <kwansk0424@gmail.com>
2021-07-21 18:19:07 +0900

    feat: Add CheckboxGroup.tsx to RegisterRoomAmenities

--- components/room/register/RegisterRoomAmenities.tsx
+++ components/room/register/RegisterRoomAmenities.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import styled from 'styled-components';
+import { amenityList } from '../../../lib/staticData';
+import { useSelector } from '../../../store';
+import { registerRoomActions } from '../../../store/registerRoom';
 import palette from '../../../styles/palette';
+import CheckboxGroup from '../../common/CheckboxGroup';
+import RegisterRoomFooter from './RegisterRoomFooter';
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -26,6 +32,14 @@ const Container = styled.div`
 `;
 
 const RegisterRoomAmenities: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const amenities = useSelector((state) => state.registerRoom.amenities);
+
+  const onChangeAmenities = (selected: string[]) => {
+    dispatch(registerRoomActions.setAmenities(selected));
+  };
+
   return (
     <Container>
       <h2>어떤 편의 시설을 제공하시나요?</h2>
@@ -34,6 +48,13 @@ const RegisterRoomAmenities: React.FC = () => {
         일반적으로 게스트가 기대하는 편의 시설 목록입니다. 숙소를 등록한 후 언제든 편의 시설을
         추가할 수 있어요.
       </p>
+      <div>
+        <CheckboxGroup value={amenities} options={amenityList} onChange={onChangeAmenities} />
+      </div>
+      <RegisterRoomFooter
+        prevHref='/room/register/geometry'
+        nextHref='/room/register/conveniences'
+      />
     </Container>
   );
 };
