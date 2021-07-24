b437ce11d33545d9d8a805428e741aa716699eb5
kwansikdev <kwansk0424@gmail.com>
2021-07-24 16:56:43 +0900

    feat: Add DatePicker

--- components/room/register/RegisterRoomDate.tsx
+++ components/room/register/RegisterRoomDate.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import DatePicker from '../../common/DatePicker';
 
 import styled from 'styled-components';
 import palette from '../../../styles/palette';
+import { useSelector } from '../../../store';
+import { useDispatch } from 'react-redux';
+import { registerRoomActions } from '../../../store/registerRoom';
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -20,10 +24,19 @@ const Container = styled.div`
 `;
 
 const RegisterRoomDate: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const startDate = useSelector((state) => state.registerRoom.startDate);
+
+  const onChangeStartDate = (date: Date | null) => {
+    dispatch(registerRoomActions.setStartDate(date ? date.toISOString() : null));
+  };
+
   return (
     <Container>
       <h2>예약 가능 여부 설정하기</h2>
       <h3>11단계</h3>
+      <DatePicker selected={startDate ? new Date(startDate) : null} onChange={onChangeStartDate} />
     </Container>
   );
 };
