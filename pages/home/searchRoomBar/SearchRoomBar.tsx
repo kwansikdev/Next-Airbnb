956d4c3ab4baa31cc1125f8f8ef106941427a065
kwansikdev <kwansk0424@gmail.com>
2021-07-28 17:20:25 +0900

    feat: Add SearchRoomBar

--- pages/home/searchRoomBar/SearchRoomBar.tsx
+++ pages/home/searchRoomBar/SearchRoomBar.tsx
@@ -15,10 +15,34 @@ const Container = styled.div`
   align-items: center;
   border-radius: 12px;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.08);
+
+  .search-room-bar-inputs {
+    display: flex;
+    align-items: center;
+    width: 100%;
+
+    .search-room-bar-input-divider {
+      background-color: ${palette.gray_dd};
+      width: 1px;
+      height: 44px;
+    }
+  }
 `;
 
 const SearchRoomBar: React.FC = () => {
-  return <Container>SearchRoomBar</Container>;
+  return (
+    <Container>
+      <div className='search-room-bar-inputs'>
+        <SearchRoomBarLocation />
+        <div className='search-room-bar-input-divider' />
+        <SearchRoomCheckInDate />
+        <div className='search-room-bar-input-divider' />
+        <SearchRoomCheckOutDate />
+        <div className='search-room-bar-input-divider' />
+        <SearchRoomGuests />
+      </div>
+    </Container>
+  );
 };
 
 export default SearchRoomBar;
